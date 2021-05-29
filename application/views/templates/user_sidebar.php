ef1aa63b07e8b816c256daf75e6b2e34d3f741af
Gian Akbar <akbarigi20@gmail.com>
2021-05-29 16:53:51 +0700

    01 - Add File CodeIgniter 3 dan Assets
    Menambahkan file CodeIgniter dan Assets

---
+++ application/views/templates/user_sidebar.php
@@ -0,0 +1,33 @@
+	<div class="navbar-custom">
+		<div class="container-fluid">
+
+			<div id="navigation">
+
+				<!-- Navigation Menu-->
+				<ul class="navigation-menu">
+
+					<li class="has-submenu">
+						<a href="/"><i class="icon-accelerator"></i> Dashboard</a>
+					</li>
+
+					<li class="has-submenu">
+						<a href="#"><i class="icon-pencil-ruler"></i> Master Absensi <i class="mdi mdi-chevron-down mdi-drop"></i></a>
+						<ul class="submenu megamenu">
+							<li>
+								<ul>
+									<li><a href="ui-alerts.html">Data Absensi</a></li>
+									<li><a href="ui-alerts.html">Data Overtime</a></li>
+									<li><a href="<?= base_url('data-cuti-karyawan'); ?>">Data Cuti</a></li>
+								</ul>
+							</li>
+						</ul>
+					</li>
+
+
+				</ul>
+				<!-- End navigation menu -->
+			</div>
+			<!-- end #navigation -->
+		</div>
+		<!-- end container -->
+	</div>
